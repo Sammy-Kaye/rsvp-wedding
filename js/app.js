4bd1a746fbc04ddc1bd5c0f1681cfffbc8088427
Samuel <samuelkaye92@gmail.com>
2025-10-31 19:08:05 +0200

    fix: re-add generateUniqueCode function

--- js/app.js
+++ js/app.js
@@ -276,6 +276,15 @@ async function submitRSVP(response) {
     }
 }
 
+function generateUniqueCode() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+}
+
 
 
 async function generateAndDownloadInvitation() {
